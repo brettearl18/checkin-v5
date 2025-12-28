406011e986842be33a00e12584d9554b69329dcc
brettearl18 <133081369+brettearl18@users.noreply.github.com>
2025-12-28 13:23:52 +0800

    Remove temporary check_onboarding_data.js file

--- check_onboarding_data.js
+++
@@ -1,5 +0,0 @@
-// Quick script to check onboarding data structure
-const admin = require('firebase-admin');
-
-// This is just to understand the structure - we'll check via API instead
-console.log('To check onboarding data, we should query the API or Firestore directly');
